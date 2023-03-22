d5861a8412e8eb5049b5282f2838eeac8f7d6570
kimYeongUng <kimyeongung92@gmail.com>
2023-03-22 14:25:53 +0900

    add operator

---
+++ src/main/java/rxjava2/operator/ObservableInterval.java
@@ -0,0 +1,16 @@
+package rxjava2.operator;
+
+import io.reactivex.rxjava3.core.Observable;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class ObservableInterval {
+    public static void main(String[] args) throws InterruptedException {
+        Observable.interval(0,1000L, TimeUnit.MILLISECONDS)
+                .map(num->num+" Count")
+                .subscribe(data -> log.info("operate: "+data));
+        Thread.sleep(2000);
+    }
+}
